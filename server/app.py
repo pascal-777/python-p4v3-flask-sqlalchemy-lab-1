a68464865d2b19d7ef8d4065f82380c51eaf724c
PascalEstime <nicintijep@gmail.com>
2024-06-29 14:39:47 +0300

    complets lab

--- server/app.py
+++ server/app.py
@@ -20,6 +20,35 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+@app.route('/earthquakes/<int:id>')
+def eathquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {
+          'id': earthquake.id,
+          'location': earthquake.location,
+          'magnitude': earthquake.magnitude,
+          'year': earthquake.year  
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(earthquake.to_dict())
+
+    body = {'count': len (quakes),
+            'quakes': quakes
+            }
+    return make_response(body, 200)
+
+
 # Add views here
 
 
